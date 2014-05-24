2518b78b5a41267bcc20eb05eb845e2049b19b69
Joshua-Pearson <j_d_pearson@yahoo.com>
2014-05-23 21:40:20 -0700

    adding ajax calls

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,9 +3,11 @@ class FlightsController < ApplicationController
   end
 
   def show
+    @flight = current_user.flights
   end
 
   def new
-    
+    @flight = Flight.new
+    @flights = Flight.all
   end
 end
