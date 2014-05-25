327e0311b12fa4d2eb78769bac9ac40f9520b205
Joshua-Pearson <j_d_pearson@yahoo.com>
2014-05-25 02:37:26 -0700

    adding mailer stuff and deleting some stuff not useful

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -7,6 +7,15 @@ class FlightsController < ApplicationController
   def create
     @flight = Flight.new flight_params
     @flight.user = current_user
+    mailer_user_address = current_user.email
+    mailer_flight_number = params["flight"]["flight_number"]
+    mailer_airline_name = params["flight"]["airline_name"]
+    mailer_departure_time = params["flight"]["departure_time"]
+    mailer_arrival_time = params["flight"]["arrival_time"]
+    mailer_departure_airport = params["flight"]["departure_airport"]
+    mailer_arrival_airport = params["flight"]["arrival_airport"]
+    mailer_departure_city = params["flight"]["departure_city"]
+    mailer_arrival_city = params["flight"]["arrival_city"]
     binding.pry
     respond_to do |format|
       if @flight.save
