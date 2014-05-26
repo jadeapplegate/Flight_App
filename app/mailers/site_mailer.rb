10e6ecffd634d8146665b83eb81078fcd77771bb
Henry Huang <hhuang2088@gmail.com>
2014-05-26 14:54:02 -0700

    dummy methods and draft templates

--- app/mailers/site_mailer.rb
+++ app/mailers/site_mailer.rb
@@ -13,4 +13,25 @@ class SiteMailer < ActionMailer::Base
     @arrival_city = a_city
     mail(subject: subject, body: body)
   end
+
+  # def contact_email (user, contact, flight) #mailer method that will send initial confirmation email to a contact
+  #   @flight = flight 
+  #   @user = user
+  #   @contact = contact 
+  #   mail(subject: "Flight stats of #{@user.first_name}")
+  # end
+
+  # def user_email(user, flight) #mailer method that will send initial confirmation email to a user
+  #   @flight = flight 
+  #   @user = user 
+  #   mail(subject: "Flight stats confirmation ")
+  # end
+
+  # def alert_email(user, contact, flight) #method that will send alert message if there's a change in flight schedule
+  #   @flight = flight 
+  #   @user = user 
+  #   @contact = contact 
+  #   mail(to: @user.email, subject: "Change in Flight schedule") 
+  #   mail(to: @contact.email, subject: "Change in Flight schedule")
+  # end
 end
