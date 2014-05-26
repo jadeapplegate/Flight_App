38977afef32ec5c119d8dd07bd1c22c49975ce97
Jade <jade.applegate@gmail.com>
2014-05-25 17:57:10 -0700

    added validations on contacts model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,9 @@
 class Contact < ActiveRecord::Base
   belongs_to :user
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :phone, presence: true
+
 end
