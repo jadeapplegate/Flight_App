c7a254379d5e78842c982d43100a1d9fb59ce24d
Jade <jade.applegate@gmail.com>
2014-05-25 18:10:04 -0700

    added validation for email format to contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,6 +4,7 @@ class Contact < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
+  validates_format_of :email, :with => /\A(\S+)@(.+)\.(\S+)\z/
   validates :phone, presence: true
 
 end
