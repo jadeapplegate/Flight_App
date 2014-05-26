94af8e44655096491710ce32209fe0535e631cd0
Jade <jade.applegate@gmail.com>
2014-05-25 17:57:38 -0700

    added rspec tests for contacts model

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
-describe Contact do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe Contact do
+       
+    describe 'first_name' do
+      it 'should not be empty' do
+        user = FactoryGirl.build(:contact, first_name: nil)
+        user.should_not be_valid
+      end
+    end
+
+    describe 'last_name' do
+      it 'should not be empty' do
+        user = FactoryGirl.build(:contact, last_name: nil)
+        user.should_not be_valid
+      end
+    end
+
+    describe 'email' do
+      it 'should not be empty' do
+        user = FactoryGirl.build(:contact, email: nil)
+        user.should_not be_valid
+      end
+    end
+
+    describe 'phone' do
+      it 'should not be empty' do
+        user = FactoryGirl.build(:contact, phone: nil)
+        user.should_not be_valid
+      end
+    end
+
 end
