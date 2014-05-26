452e4330772b171d31a66265c1878671037ad747
Jade <jade.applegate@gmail.com>
2014-05-25 18:42:10 -0700

    added rspec tests for first and last name minimum length

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -7,12 +7,22 @@ require 'spec_helper'
         user = FactoryGirl.build(:contact, first_name: nil)
         user.should_not be_valid
       end
+
+      it 'should contain at least 2 characters' do
+        user = FactoryGirl.build(:contact, first_name: "E").should_not be_valid
+        user = FactoryGirl.build(:contact, first_name: "Ed").should be_valid
+      end
     end
 
     describe 'last_name' do
       it 'should not be empty' do
         user = FactoryGirl.build(:contact, last_name: nil)
         user.should_not be_valid
+      end
+
+      it 'should contain at least 2 characters' do
+        user = FactoryGirl.build(:contact, last_name: "L").should_not be_valid
+        user = FactoryGirl.build(:contact, last_name: "Li").should be_valid
       end
     end
 
@@ -36,7 +46,7 @@ require 'spec_helper'
       end
 
 
-   describe "when email format is invalid" do
+   describe "email format" do
       it "should be invalid" do
         addresses = %w[user@foo,com user_at_foo.org example.user@foo.
                        foo@bar_baz.com foo@bar+baz.com]
