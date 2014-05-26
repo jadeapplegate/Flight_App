e24af2cc210ed61c8c403cefcd02caa58920e274
Jade <jade.applegate@gmail.com>
2014-05-25 17:30:07 -0700

    added factory data

---
+++ spec/factories/factories.rb
@@ -0,0 +1,33 @@
+
+FactoryGirl.define do
+
+  sequence :email do |n|
+    "mickey#{n}@mouse.com"
+  end
+
+  factory :user do
+    first_name "Mickey"
+    last_name "Mouse"
+    email 
+    password "password"
+    password_confirmation "password"
+  end
+
+  factory :flight do
+    flight_number "902"
+    airline_code "VX"
+    date_month "06"
+    date_day "03"
+    date_year "2014"
+  end
+
+  factory :contact do
+    first_name "Daffy"
+    last_name "Duck"
+    email
+    phone "555-555-5555"
+  end
+
+end
+
+
