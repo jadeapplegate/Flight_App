b8c68f72bf7a726650270a71c0680a9b4c1833de
Jade <jade.applegate@gmail.com>
2014-05-25 18:20:34 -0700

    added length_of validations for first and last name to contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,7 +2,9 @@ class Contact < ActiveRecord::Base
   belongs_to :user
 
   validates :first_name, presence: true
+  validates_length_of :first_name, :minimum => 2
   validates :last_name, presence: true
+  validates_length_of :last_name, :minimum => 2
   validates :email, presence: true
   validates_format_of :email, :with => /\A(\S+)@(.+)\.(\S+)\z/
   validates :phone, presence: true
