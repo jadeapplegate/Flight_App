d31745ebb68db2fc717437810bb053a51492534c
Joshua-Pearson <j_d_pearson@yahoo.com>
2014-05-27 11:34:49 -0700

    adding jade's updates

--- app/workers/emails_worker.rb
+++ app/workers/emails_worker.rb
@@ -2,7 +2,21 @@ class EmailsWorker
   include Sidekiq::Worker
   sidekiq_options retry: false
   
-  def perform(address, number, name, d_time, a_time, d_airport, a_airport, d_city, a_city, current_user_id)
-    SiteMailer.flight_email(address, number, name, d_time, a_time, d_airport, a_airport, d_city, a_city, current_user_id).deliver
+  def perform_contact_email(address, flight_id, current_user_id)
+    SiteMailer.flight_email(address, flight_id, current_user_id).deliver
+  end
+
+  def perform_user_email
+    SiteMailer.
+  end
+
+  recurrance { daily }
+
+  def perform_user_twillio
+    @flights = Flight.all
+
+  end
+
+  def perform_contact_twillio
   end
 end
