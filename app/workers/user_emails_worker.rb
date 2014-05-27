1d8a15949455bb742f3a882acba5aa5c86f0c3b7
Henry Huang <hhuang2088@gmail.com>
2014-05-27 12:05:36 -0700

    mailer view added back

--- app/workers/user_emails_worker.rb
+++ app/workers/user_emails_worker.rb
@@ -2,11 +2,7 @@ class UserEmailsWorker
   include Sidekiq::Worker
   sidekiq_options retry: false
   
-<<<<<<< HEAD
   def perform(flight_id, current_user_id)
-=======
-  def perform_contact_email(address, flight_id, current_user_id)
     SiteMailer.confirm_user(flight_id, current_user_id).deliver
->>>>>>> corrected user_email_worker, and formatted confirm_user method parameters
   end
 end
