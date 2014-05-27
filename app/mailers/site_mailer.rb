466b2028a5bfcfbe11369bb458b9af8239b3d6b0
Joshua-Pearson <j_d_pearson@yahoo.com>
2014-05-27 10:34:15 -0700

    refactoring controller for email

--- app/mailers/site_mailer.rb
+++ app/mailers/site_mailer.rb
@@ -1,16 +1,9 @@
 class SiteMailer < ActionMailer::Base
   default from: ENV['EMAIL_FROM_ADDRESS']
   
-  def flight_email(address, number, name, d_time, a_time, d_airport, a_airport, d_city, a_city, current_user_id)
+  def flight_email(address, flight_id, current_user_id)
     @current_user = User.find(current_user_id)
-    @number = number
-    @name = name
-    @departure_time = d_time
-    @arrival_time = a_time
-    @departure_airport = d_airport
-    @arrival_airport = a_airport
-    @departure_city = d_city
-    @arrival_city = a_city
+    @flight = Flight.find(flight_id)
     mail(to: address)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140525022005) do
+ActiveRecord::Schema.define(version: 20140526220611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,10 +42,10 @@ ActiveRecord::Schema.define(version: 20140525022005) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "airline_code"
     t.integer  "contact_id"
     t.string   "departure_city"
     t.string   "arrival_city"
+    t.string   "airline_code"
   end
 
   add_index "flights", ["user_id"], name: "index_flights_on_user_id", using: :btree
