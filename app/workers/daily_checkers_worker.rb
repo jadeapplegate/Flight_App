588e1d319148ddc2841977630f77c81d834d7e97
Joshua-Pearson <j_d_pearson@yahoo.com>
2014-05-28 10:41:54 -0700

    sidetiq works daily email

--- app/workers/daily_checkers_worker.rb
+++ app/workers/daily_checkers_worker.rb
@@ -3,10 +3,22 @@ class DailyCheckersWorker
   include Sidetiq::Schedulable
 
   recurrence backfill: true do
-    daily.hour_of_day(10).minute_of_hour(1)
+    daily.hour_of_day(0).minute_of_hour(1)
   end
   
   def perform
-    flights_today
+    todays_flights = Flight.where("departure_time BETWEEN ? AND ?", DateTime.current.beginning_of_day, DateTime.tomorrow.beginning_of_day)
+    if todays_flights.empty? == false
+      todays_flights.each do |flight|
+        flight_id = flight.id 
+        todays_contacts = ContactsFlights.where(["flight_id = ?", flight_id]).map { |flight| flight.contact }
+        if todays_contacts.empty? == false
+          todays_contacts.each do |contact|
+            contact_id = contact.id
+            DailyEmailsWorker.perform_async(flight_id, contact_id)
+          end
+        end
+      end
+    end
   end
 end
