ce49cd2770fdbc756ad2f60cae8ae6c80eddfba0
Joshua-Pearson <j_d_pearson@yahoo.com>
2014-05-29 12:49:30 -0700

    authorization views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
   
   def profile
-    @flight = Flight.new
-    @flights = current_user.flights
-    @contacts = current_user.contacts
-    @contact = Contact.new
+    if current_user
+      @flight = Flight.new
+      @flights = current_user.flights
+      @contacts = current_user.contacts
+      @contact = Contact.new
+    else
+      redirect_to root_path
+      flash[:error] = "Please sign in or sign up"
+    end
   end
 
 end
